70f01a43a5266bf2d0850668e34e7229bc8c8a12
lengleng12138 <37523147+lengleng12138@users.noreply.github.com>
2018-05-19 16:27:03 +0800

    Delete 1.js

--- picture/1.js
+++
@@ -1,9 +0,0 @@
-function showpic(whichpic){
- var source = whichpic.getAttribute("href");
- var placeholder = document.getElementById("placeholder");
- placeholder.setAttribute("src",source);
- var text = whichpic.getAttribute("title");
- var description = document.getElementById("description");
- description.firstChild.nodeValue = text;
- 
-} 
