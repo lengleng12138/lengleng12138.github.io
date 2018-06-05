19651b6663745cc15470921c646265a6c6b76439
lengleng12138 <37523147+lengleng12138@users.noreply.github.com>
2018-06-05 18:42:38 +0800

    Add files via upload

---
+++ 05.21/picture.js
@@ -0,0 +1,63 @@
+function showPic(whichpic){
+ if (!document.getElementById("placeholder")) return false;
+ var source = whichpic.getAttribute("href");
+ var placeholder = document.getElementById("placeholder");
+ if (placeholder.nodeName != "IMG") return false;
+ placeholder.setAttribute("src",source);
+ var text = whichpic.getAttribute("title");
+ var description = document.getElementById("description");
+ description.firstChild.nodeValue = text;
+}
+function prepareGallery(){
+ if(!document.getElementsByTagName) return false;
+ if(!document.getElementById) return false;
+ if(!document.getElementById("imagegallery")) return false;
+ var gallery = document.getElementById("imagegallery");
+ var links = gallery.getElementsByTagName("a");
+ for (var i=0;i<links.length;i++){
+  links[i].onclick = function(){
+   showPic(this); 
+   return false ;
+  }
+ }
+}
+function insertAfter(newElement,targetElement){
+ var parent = targetElement.parentNode;
+ if (parent.lastChild == targetElement){
+  parent.appendChild(newElement);
+ }else{
+  parent.insertBefore(newElement,targetElement.nextSibling);
+ }
+}
+function preparePlaceholder(){
+ if (!document.createElement) return false;
+ if (!document.createTextNode) return false;
+ if (!document.getElementById) return false;
+ if (!document.getElementById("imagegallery")) return false;
+ var placeholder = document.createElement("img");
+ placeholder.setAttribute("id","placeholder");
+ placeholder.setAttribute("src","haijing.jpg");
+ placeholder.setAttribute("alt","my image gallery");
+ var description = document.createElement("p");
+ description.setAttribute("id","description");
+ var desctext = document.createTextNode("Choose an image.");
+ description.appendChild(desctext);
+ var gallery = document.getElementById("imagegallery");
+ insertAfter(placeholder,gallery);
+ insertAfter(description,placeholder);
+}
+
+function addLoadEvent(func){
+ var oldonload = window.onload;
+ if(typeof window.onload != 'function'){
+  window.onload = func;
+ }
+ else {
+  window.onload = function(){
+   oldonload();
+   func();
+  }
+ }
+}
+addLoadEvent(prepareGallery);
+addLoadEvent(preparePlaceholder);
